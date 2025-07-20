01af2d4f39a6079e355a184db5ce8611f8be0cb8
NadeeraPereraD <98936438+NadeeraPereraD@users.noreply.github.com>
2025-07-20 18:26:17 +0530

    Commit before upgrade packges

---
+++ AMDT/AMDT.UI/src/environments/environment.prod.ts
@@ -0,0 +1,5 @@
+export const environment = {
+    production: true,
+    authBaseUrl: 'http://10.40.47.30:8086/api',
+    LocalhostUrl: 'https://10.40.47.30:8097/api',
+};
---
+++ AMDT/AMDT.UI/src/environments/environment.ts
@@ -0,0 +1,5 @@
+export const environment = {
+    production: false,
+    authBaseUrl: 'http://10.40.47.30:8097/api',
+    LocalhostUrl: 'https://10.40.47.30:8097/api',
+};
